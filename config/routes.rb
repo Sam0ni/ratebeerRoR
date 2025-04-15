de2f19989989ba410d5d35f9bc563c7a01350562
Samppa Salo <samppasalo16@gmail.com>
2025-04-15 17:01:20 +0300

    add week3 exercise 4

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   root 'breweries#index'
   get 'kaikki_bisset', to: 'beers#index'
   resources :ratings, only: [:index, :new, :create, :destroy]
+  get "signin", to: "sessions#new"
+  delete "signout", to: "sessions#destroy"
+  resource :session, only: [:new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
